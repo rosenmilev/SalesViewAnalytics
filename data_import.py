23b0e22894569a7fc2609b67ce39ab5f0e64be92
traxx90 <rosenmilev@gmail.com>
2023-12-15 14:39:43 +0200

    Fix data cleaning logic,add database schema creation logic and fill the cleaned data.

--- data_import.py
+++ data_import.py
@@ -1,25 +1,25 @@
 import pandas as pd
-import numpy as np
-
 
 
 def read_data(file_path):
     data = pd.read_csv(file_path)
+    data.columns = data.columns.str.strip()
     return data
 
 
 class CleanData:
     def __init__(self, dataframe):
-        self.dataframe = dataframe
-        # TODO Move setting of these values outside class, user input preferably.
+        self.dataframe = dataframe.copy()
+        # TODO Move setting of these values outside class dynamically.
+        self.rows_dropped = 0
         self.critical_columns = ['Order ID', 'City', 'State', 'Postal Code', 'Product ID', 'Sales', 'Country',
-                                 'Order Date', 'Ship Date', 'Customer ID', 'Customer Name', 'Row ID', 'Product Name']
+                                 'Order Date', 'Ship Date', 'Customer ID', 'Customer Name', 'Product Name']
         self.categorical_columns = ['Ship Mode', 'Segment', 'Category', 'Sub-Category']
         self.columns_to_drop = ['Row ID']
         self.column_type_map = {'Order Date': 'datetime',
                                 'Ship Date': 'datetime',
-                                'Sales': 'float',
-                                'Postal Code': 'int',
+                                'Sales': 'float64',
+                                'Postal Code': 'str',
                                 'Ship Mode': 'str',
                                 'Customer Name': 'str',
                                 'Country': 'str',
@@ -35,11 +35,11 @@ class CleanData:
 
     def main(self):
         if self.calculate_missing_values_percentage() <= 30:
+            self.remove_redundant_columns()
             self.handling_missing_values()
-            for col in self.dataframe.columns.values:
-                self.remove_redundant_columns(col)
-                if col:
-                    self.correct_datatypes(col)
+            self.correct_datatypes()
+            self.drop_nan_rows()
+            self.format_column_name()
 
     # What percent of data is missing and deside if the dataset is suitable for analysis.
     # TODO implement logic not to accept datasets with more than 30% missing values.
@@ -56,39 +56,55 @@ class CleanData:
     # TODO Think how to provide to user decision how to handle missing values and which columns to drop.
     def handling_missing_values(self):
         # Handle the critical columns by deleting the rows with missing data.
-        self.dataframe = self.dataframe.dropna(subset=self.critical_columns)
+        self.dataframe = self.dataframe.dropna(subset=self.critical_columns).copy()
         # Handle the categorical columns by filling them with most common value in each column.
         for column in self.categorical_columns:
             mode_value = self.dataframe[column].mode()[0]
             self.dataframe.loc[:, column] = self.dataframe[column].fillna(mode_value)
 
-    # Remove all the redundant columns
-    def remove_redundant_columns(self, column_name):
-        # Drop user defined columns since they are not needed for db design.
-        self.dataframe.drop(columns=self.columns_to_drop, inplace=True, errors='ignore')
+    # Remove all the redundant columns, selected by user
+    def remove_redundant_columns(self):
+        print(self.dataframe.columns)
+        for column in self.columns_to_drop:
+            if column in self.dataframe.columns:
+                self.dataframe.drop(columns=column, inplace=True)
+            else:
+                print(f'Column {column} not found')
+        unnamed_columns = [col for col in self.dataframe.columns if col.startswith('Unnamed')]
+        if unnamed_columns:
+            self.dataframe.drop(columns=unnamed_columns, inplace=True)
+        print("Columns after dropping:", self.dataframe.columns)
 
-    def correct_datatypes(self, column_name):
-        if column_name == 'Order Date' or column_name == 'Ship Date':
-            self.dataframe[column_name] = pd.to_datetime(self.dataframe[column_name])
-        elif column_name == 'Sales':
-            self.dataframe[column_name] = self.dataframe[column_name].astype(float)
-        elif column_name == 'Postal Code':
-            self.dataframe[column_name] = self.dataframe[column_name].astype(int)
-        else:
-            self.dataframe[column_name] = self.dataframe[column_name].astype(str)
+    # Assign correct datatype for each column, change the date format to match SQLite one.
+    def correct_datatypes(self):
+        for col, data_type in self.column_type_map.items():
+            try:
+                if data_type == 'datetime':
+                    self.dataframe[col] = pd.to_datetime(self.dataframe[col], format='%d/%m/%Y', errors='coerce').dt.strftime('%Y-%m-%d')
+                elif data_type in ['float64', 'int32']:
+                    self.dataframe[col] = pd.to_numeric(self.dataframe[col], errors='coerce')
+                elif data_type == 'str':
+                    self.dataframe[col] = self.dataframe[col].astype(str)
+                # No need to use copy() here since direct assignment is being used
+            except Exception as e:
+                print(f"Error converting column {col}: {e}")
+
+    def drop_nan_rows(self):
+        starting_row_count = len(self.dataframe)
+        self.dataframe = self.dataframe.dropna().copy()
+        final_row_count = len(self.dataframe)
+        if starting_row_count != final_row_count:
+            self.rows_dropped = starting_row_count - final_row_count
+            print(f"Rows dropped: {self.rows_dropped}")
+
+    # Format all the column names to consist only from lower case and replaces all not alnum symbols to _
+    def format_column_name(self):
+        self.dataframe.columns = self.dataframe.columns.str.lower().str.replace('[^a-zA-Z0-9]', '_', regex=True)
+
 
 
 data = read_data('sales_dataset.csv')
-
-handle_data = CleanData(data)
-
-print(handle_data.calculate_missing_values_percentage())
-# handle_data.remove_redundant_columns()
-handle_data.handling_missing_values()
-cleaned_data = handle_data.dataframe
-print(handle_data.calculate_missing_values_percentage())
-print(handle_data.dataframe.dtypes)
-
-
-if __name__ == '__main__':
-    pass
+cleaner = CleanData(data)
+cleaner.main()
+cleaned_data = cleaner.dataframe
+cleaned_data.to_csv('sales_dataset_cleaned.csv', index=False)
--- database_utils.py
+++ database_utils.py
@@ -9,6 +9,7 @@ def create_path(db_filename):
     db_filepath = os.path.join(current_directory, db_filename)
     return db_filepath
 
+
 def create_connection(file_name):
     conn = None
     db_filepath = create_path(file_name)
@@ -27,58 +28,84 @@ def create_table(connection, sql_for_table):
         print(e)
 
 
+def insert_data_into_table(connection, df, table_name):
+    try:
+        df.to_sql(table_name, connection, if_exists='append', index=False)
+    except Error as e:
+        print(f"Error inserting data into {table_name}: {e}")
+
+def generate_sql():
+    pass
+
+
 # Using normalized database schema
 def main():
-    regions_table_sql = '''CREATE TABLE IF NOT EXISTS Regions (
-    postal_code INTEGER PRIMARY KEY,
-    city TEXT,
-    state TEXT,
-    country TEXT,
-    region TEXT
-    );
-
+    customers_table_sql = '''CREATE TABLE IF NOT EXISTS customers (
+    customer_id TEXT PRIMARY KEY,
+    customer_name TEXT NOT NULL,
+    segment TEXT NOT NULL
+    )
     '''
 
-    customers_table_sql = '''CREATE TABLE IF NOT EXISTS Customers (
-    int_customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    customer_id TEXT UNIQUE NOT NULL,
-    customer_name TEXT,
-    segment TEXT,
-    postal_code INTEGER NOT NULL,
-    FOREIGN KEY(postal_code) REFERENCES Regions(postal_code)
-    );
-    '''
-
-    products_table_sql = '''CREATE TABLE IF NOT EXISTS Products (
-    int_product_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    product_id TEXT UNIQUE NOT NULL,
-    category TEXT,
-    sub_category TEXT,
+    products_table_sql = '''CREATE TABLE IF NOT EXISTS products (
+    product_id TEXT PRIMARY KEY,
+    category TEXT NOT NULL,
+    sub_category TEXT NOT NULL,
     product_name TEXT NOT NULL 
-    );'''
+    )'''
 
-    sales_table_sql = ''' CREATE TABLE IF NOT EXISTS Orders(
-    int_order_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    order_id TEXT UNIQUE NOT NULL,
-    order_date DATE,
+    sales_transactions_table_sql = ''' CREATE TABLE IF NOT EXISTS sales_transactions (
+    order_id TEXT PRIMARY KEY ,
+    order_date DATE NOT NULL,
     ship_date DATE,
-    ship_mode TEXT,
-    int_customer_id INTEGER,
-    int_product_id INTEGER,
-    sales REAL NOT NULL ,
-    FOREIGN KEY(int_customer_id) REFERENCES Customers(int_customer_id),
-    FOREIGN KEY(int_product_id) REFERENCES Products(int_product_id)
-    );'''
+    ship_mode TEXT NOT NULL,
+    customer_id TEXT NOT NULL,
+    city TEXT NOT NULL,
+    region TEXT NOT NULL,
+    postal_code TEXT NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+    )'''
 
+    order_details_table_sql = '''CREATE TABLE IF NOT EXISTS order_details (
+    order_details_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    sales REAL NOT NULL,
+    FOREIGN KEY(order_id) REFERENCES sales_transactions(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    )'''
+
+    # Create table, filter df for each table, insert the data
     file_name = 'db.sqlite3'
     connection = create_connection(file_name)
-    create_table(connection, regions_table_sql)
     create_table(connection, customers_table_sql)
     create_table(connection, products_table_sql)
-    create_table(connection, sales_table_sql)
+    create_table(connection, sales_transactions_table_sql)
+    create_table(connection, order_details_table_sql)
+    print('Tables Created')
+
+    customers_df = cleaned_data.copy()[['customer_id', 'customer_name', 'segment']]
+    customers_df = customers_df.drop_duplicates(subset=['customer_id'])
+
+    products_df = cleaned_data.copy()[['product_id', 'category', 'sub_category', 'product_name']]
+    products_df = products_df.drop_duplicates(subset=['product_id'])
+
+    sales_transactions_df = cleaned_data.copy()[['order_id', 'order_date', 'ship_date', 'ship_mode', 'customer_id',
+                                                 'city', 'region', 'postal_code']]
+    sales_transactions_df = sales_transactions_df.drop_duplicates(subset=['order_id'])
+
+    order_details_df = cleaned_data.copy()[['order_id', 'product_id', 'sales']]
+
+    insert_data_into_table(connection, customers_df, 'customers')
+    insert_data_into_table(connection, products_df, 'products')
+    insert_data_into_table(connection, sales_transactions_df, 'sales_transactions')
+    insert_data_into_table(connection, order_details_df, 'order_details')
+    print('Tables Inserted')
+
+    if connection:
+        connection.close()
 
 
 if __name__ == '__main__':
-
-    con = create_connection('db.sqlite3')
+    main()
 
